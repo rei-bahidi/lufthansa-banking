6b81ab4d65a2f4bcb386353ddbc1b40c4833b74a
Rei Bahidi <rei.bahidi@cyberooglobal.com>
2024-10-10 17:28:04 +0200

    Small fix

--- lufthansa_banking/users/urls.py
+++ lufthansa_banking/users/urls.py
@@ -2,8 +2,9 @@ from django.urls import path, include
 from .views import UserListView, UserView, BankerListView, BankerView
 
 urlpatterns = [
-    path('customers/list', UserListView.as_view()),  # Link the function-based view to a URL
-    path('customers/<int:user_id>', UserView.as_view(), name='user-detail'),  # Link the class-based view to a URL for detail view
+    path('customers/', UserView.as_view(), name='user-create'),  
+    path('customers/list', UserListView.as_view(), name='user-list'), 
+    path('customers/<int:user_id>', UserView.as_view(), name='user-detail'), 
     path('bankers/list', BankerListView.as_view()),
     path('bankers/<int:user_id>', BankerView.as_view(), name='banker-detail'),
     # path('accounts/', include('django.contrib.auth.urls')),  
