1e1f25ecb397ce1561dba8dd5cf4d7fd99a6fce4
reibbb <reibuissacc@gmail.com>
2025-02-01 12:31:16 +0100

    Dummy commit

--- lufthansa_banking/users/serializers.py
+++ lufthansa_banking/users/serializers.py
@@ -6,8 +6,6 @@ from rest_framework.exceptions import ValidationError
 
 
 class CustomUserSerializer(ModelSerializer):
-
-
     class Meta:
         model = CustomUser
         fields = ['id', 'username', 'first_name', 'last_name', 'email', 'password', 'type']
